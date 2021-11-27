3bec37165bc9bc4df7e58fd6077fb1c916bde710
Wojciech Malarski <w.malarski@gmail.com>
2021-11-27 15:56:46 +0100

    removing demo folder and fixing lint erros

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,13 +1,13 @@
 import { renderToString } from "react-dom/server";
-import { RemixServer } from "remix";
 import type { EntryContext } from "remix";
+import { RemixServer } from "remix";
 
 export default function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
   remixContext: EntryContext
-) {
+): Response {
   const markup = renderToString(
     <RemixServer context={remixContext} url={request.url} />
   );
