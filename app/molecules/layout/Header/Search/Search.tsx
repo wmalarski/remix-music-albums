c934b38c06a9598d83ff3380975db48024eeee90
Wojciech Malarski <w.malarski@gmail.com>
2021-12-12 15:38:49 +0100

    moving stuff around in autocomplete search

--- app/molecules/layout/Header/Search/Search.tsx
+++ app/molecules/layout/Header/Search/Search.tsx
@@ -1,99 +1,34 @@
-import { useCombobox } from "downshift";
 import { ReactElement } from "react";
 import { useFetcher, useNavigate } from "remix";
 import { SearchQuery } from "~/api/types.server";
-import {
-  Autocomplete,
-  AutocompleteContent,
-  AutocompleteItem,
-  AutocompleteLabel,
-  AutocompleteTrigger,
-  IconButton,
-  TextInput,
-} from "~/components";
 import { routes } from "~/utils/routes";
+import { SearchAutocomplete } from "./SearchAutocomplete/SearchAutocomplete";
 
 export const Search = (): ReactElement => {
   const fetcher = useFetcher<SearchQuery>();
   const navigate = useNavigate();
 
-  const artistShift = fetcher.data?.album.length ?? 0;
-  const inputItems = [
-    ...(fetcher.data?.album ?? []),
-    ...(fetcher.data?.artist ?? []),
-  ];
+  const handleInputValueChange = (inputValue?: string) => {
+    fetcher.submit(new URLSearchParams({ query: `%${inputValue}%` }), {
+      method: "get",
+      action: routes.search,
+    });
+  };
 
-  const {
-    isOpen,
-    getToggleButtonProps,
-    getMenuProps,
-    getInputProps,
-    getComboboxProps,
-    highlightedIndex,
-    getItemProps,
-  } = useCombobox({
-    items: inputItems,
-    onInputValueChange: ({ inputValue }) => {
-      fetcher.submit(new URLSearchParams({ query: `%${inputValue}%` }), {
-        method: "get",
-        action: routes.search,
-      });
-    },
-    onSelectedItemChange: (changes) => {
-      const item = changes.selectedItem;
-      if (!item) return;
+  const handleSelectedAlbumChange = (id: number) => {
+    navigate(routes.album(id));
+  };
 
-      switch (item.__typename) {
-        case "album": {
-          navigate(routes.album(item.id));
-          return;
-        }
-        case "artist": {
-          navigate(routes.artist(item.id));
-          return;
-        }
-      }
-    },
-  });
+  const handleSelectedArtistChange = (id: number) => {
+    navigate(routes.artist(id));
+  };
 
   return (
-    <Autocomplete>
-      <AutocompleteTrigger {...getComboboxProps()}>
-        <TextInput {...getInputProps()} />
-        <IconButton
-          type="button"
-          {...getToggleButtonProps()}
-          aria-label="toggle menu"
-        >
-          &#8595;
-        </IconButton>
-      </AutocompleteTrigger>
-      <AutocompleteContent {...getMenuProps()}>
-        {isOpen && (
-          <>
-            <AutocompleteLabel>Albums</AutocompleteLabel>
-            {fetcher.data?.album.map((item, index) => (
-              <AutocompleteItem
-                active={highlightedIndex === index}
-                key={`album-${item.id}`}
-                {...getItemProps({ item, index })}
-              >
-                {`${item.title} - ${item.artistByArtist.name}`}
-              </AutocompleteItem>
-            ))}
-            <AutocompleteLabel>Artists</AutocompleteLabel>
-            {fetcher.data?.artist.map((item, index) => (
-              <AutocompleteItem
-                active={highlightedIndex === index + artistShift}
-                key={`artist-${item.id}`}
-                {...getItemProps({ item, index: index + artistShift })}
-              >
-                {item.name}
-              </AutocompleteItem>
-            ))}
-          </>
-        )}
-      </AutocompleteContent>
-    </Autocomplete>
+    <SearchAutocomplete
+      onInputValueChange={handleInputValueChange}
+      onSelectedAlbumChange={handleSelectedAlbumChange}
+      onSelectedArtistChange={handleSelectedArtistChange}
+      data={fetcher.data}
+    />
   );
 };
---
+++ app/molecules/layout/Header/Search/SearchAutocomplete/SearchAutocomplete.tsx
@@ -0,0 +1,98 @@
+import { useCombobox } from "downshift";
+import { ReactElement } from "react";
+import { SearchQuery } from "~/api/types.server";
+import {
+  Autocomplete,
+  AutocompleteContent,
+  AutocompleteItem,
+  AutocompleteLabel,
+  AutocompleteTrigger,
+  IconButton,
+  TextInput,
+} from "~/components";
+
+type SearchAutocompleteProps = {
+  data?: SearchQuery;
+  onInputValueChange: (inputValue?: string) => void;
+  onSelectedAlbumChange: (id: number) => void;
+  onSelectedArtistChange: (id: number) => void;
+};
+
+export const SearchAutocomplete = ({
+  data,
+  onInputValueChange,
+  onSelectedAlbumChange,
+  onSelectedArtistChange,
+}: SearchAutocompleteProps): ReactElement => {
+  const artistShift = data?.album.length ?? 0;
+  const inputItems = [...(data?.album ?? []), ...(data?.artist ?? [])];
+
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => onInputValueChange(inputValue),
+    onSelectedItemChange: (changes) => {
+      const item = changes.selectedItem;
+      if (!item) return;
+
+      switch (item.__typename) {
+        case "album": {
+          onSelectedAlbumChange(item.id);
+          return;
+        }
+        case "artist": {
+          onSelectedArtistChange(item.id);
+          return;
+        }
+      }
+    },
+  });
+
+  return (
+    <Autocomplete>
+      <AutocompleteTrigger {...getComboboxProps()}>
+        <TextInput {...getInputProps()} />
+        <IconButton
+          type="button"
+          {...getToggleButtonProps()}
+          aria-label="toggle menu"
+        >
+          &#8595;
+        </IconButton>
+      </AutocompleteTrigger>
+      <AutocompleteContent {...getMenuProps()}>
+        {isOpen && (
+          <>
+            <AutocompleteLabel>Albums</AutocompleteLabel>
+            {data?.album.map((item, index) => (
+              <AutocompleteItem
+                active={highlightedIndex === index}
+                key={`album-${item.id}`}
+                {...getItemProps({ item, index })}
+              >
+                {`${item.title} - ${item.artistByArtist.name}`}
+              </AutocompleteItem>
+            ))}
+            <AutocompleteLabel>Artists</AutocompleteLabel>
+            {data?.artist.map((item, index) => (
+              <AutocompleteItem
+                active={highlightedIndex === index + artistShift}
+                key={`artist-${item.id}`}
+                {...getItemProps({ item, index: index + artistShift })}
+              >
+                {item.name}
+              </AutocompleteItem>
+            ))}
+          </>
+        )}
+      </AutocompleteContent>
+    </Autocomplete>
+  );
+};
